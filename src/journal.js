f8a014d011c2e00bd08743083a275e8bf2ba861d
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-09 12:01:04 -0800

    Added js functions. Modified html.
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -1,6 +1,17 @@
-function countWords(str) {
-  let count = str.split(' ').length;
+Journal.prototype.countWords = function(){
+  let count = this.entry.split(' ').length;
   return count;
 }
 
 console.log(countWords("This is a test"));
+
+
+function Journal(entry, name) {
+  this.entry = entry;
+  this.name = name;
+}
+
+const entries = new Journal([
+  ['entry'],
+  ['name']
+]);
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import Journal from './journal.js';
+
